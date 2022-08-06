518c0bf5565de06feb28653c570abf824a452bbf
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2022-08-06 11:47:20 +0000

    Ejercicios tema 4 primera parte

---
+++ ejerciciostema4/src/EjerciciosTema4.java
@@ -0,0 +1,12 @@
+public class EjerciciosTema4 {
+    public static void main(String[] args)  {
+        int numeroIf = -2;
+        if (numeroIf == 0) {
+            System.out.println("Es cero");
+        } else if (numeroIf > 0) {
+            System.out.println("Es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("Es negativo");
+        }
+    }
+}

c8530657c53f13fabd498bbd9c97b83f16f03596
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2022-11-02 16:40:31 +0000

    ejerciciostema9

--- ejerciciostema9/src/App.java
+++ ejerciciostema9/src/App.java
@@ -10,9 +10,8 @@ public class App {
         trabajador.telefono=687654321;
         trabajador.nombre="Juan";
         trabajador.salario=2300;
-        System.out.println (cliente.nombre);
-
-
+        System.out.println ("El cliente " cliente.nombre " tiene" cliente.edad " años, su teléfono es" cliente.telefono " y tiene un crédito de" cliente.credito " €." );
+        System.out.println ("El trabajador " trabajador.nombre " tiene" trabajador.edad " años, su teléfono es" trabajador.telefono " y tiene un salario de" trabajador.salario " €." );
     }
     class Persona {
         int edad;

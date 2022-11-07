d19e22b76b283f10671d93ba8ff67bbc6d5c2d76
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2022-11-07 15:42:29 +0000

    ejercicios tema 9

--- ejerciciostema9/README.md
+++ ejerciciostema9/README.md
@@ -1,18 +1,12 @@
-## Getting Started
+Crea una clase Persona con las siguientes variables:
+La edad
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+El nombre
 
-## Folder Structure
+El teléfono
 
-The workspace contains two folders by default, where:
+Una vez creada la clase, crea una nueva clase Cliente que herede de Persona, esta nueva clase tendrá la variable credito solo para esa clase.
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+Crea ahora un objeto de la clase Cliente que debe tener como propiedades la edad, el telefono, el nombre y el credito, tienes que darles valor y mostrarlas por pantalla.
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Una vez hecho esto, haz lo mismo con la clase Trabajador que herede de Persona, y con una variable salario que solo tenga la clase Trabajador.
--- ejerciciostema9/src/App.java
+++ ejerciciostema9/src/App.java
@@ -5,23 +5,28 @@ public class App {
         cliente.telefono=612345678;
         cliente.nombre="Antonio";
         cliente.credito=1000;
+
         Trabajador trabajador = new Trabajador();
         trabajador.edad=35;
         trabajador.telefono=687654321;
         trabajador.nombre="Juan";
         trabajador.salario=2300;
-        System.out.println ("El cliente " cliente.nombre " tiene" cliente.edad " años, su teléfono es" cliente.telefono " y tiene un crédito de" cliente.credito " €." );
-        System.out.println ("El trabajador " trabajador.nombre " tiene" trabajador.edad " años, su teléfono es" trabajador.telefono " y tiene un salario de" trabajador.salario " €." );
+        
+        System.out.println ("El cliente " + cliente.nombre + " tiene " + cliente.edad + " años, su teléfono es " + cliente.telefono + " y tiene un crédito de " + cliente.credito + " €." );
+        System.out.println ("El trabajador " + trabajador.nombre + " tiene " + trabajador.edad + " años, su teléfono es " + trabajador.telefono + " y tiene un salario de " + trabajador.salario + " €." );
     }
-    class Persona {
-        int edad;
-        String nombre;
-        int telefono;
-    }
-    class Cliente extends Persona {
-        int credito;
-    }
-    class Trabajador extends Persona {
-        int salario;
-    }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    int telefono;
+}
+
+class Cliente extends Persona {
+    int credito;
+}
+
+class Trabajador extends Persona {
+    int salario;
 }
